f0c4af52b08425b54fcebf45369f243a8665f9e9
Alex <contact.alexandremace@gmail.com>
2021-12-19 07:26:13 +0100

    Add basic style and dynamic projects config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const tools = [
+      {
+          title: 'Footprint',
+          description: 'Dynamic carbon footprint (CO2eq) of individual actions data visualization, using Ademe base carbone\'s data.',
+          logo: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/foot_1f9b6.png',
+          backgroundColor: '#E7E1E1',
+          color: '#ff4b31',
+          link: 'https://alexandre-mace.github.io/footprint/'
+      }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="container header">
+            <div className="row align-items-center">
+                <div className="col d-flex justify-content-between">
+                    <a href={"/"} className={"d-flex align-items-center"}>
+                            <img className={"logo-img mr-3"} src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/thermometer_1f321-fe0f.png" alt="Thermomètre"/> ClimateTracker
+                    </a>
+                    <div>
+                        <div className={"header-links-trigger"}>
+                            <div className={"d-flex align-items-center"}>
+                                <div className={"mr-2"}>Les outils</div>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 6 6"
+                                     className="Navigation_pageArrow__3E5fm" role="presentation">
+                                    <path d="M.371 2.995A.6.6 0 01.822 2h4.356a.6.6 0 01.451.995L3.602 5.312a.8.8 0 01-1.204 0z"
+                                          fill="currentColor"></path>
+                                </svg>
+                            </div>
+                            <div className="header-links-wrapper">
+                                <ul className={"header-links"}>
+                                    {tools.map(tool => (
+                                        <li>
+                                            <a target={"blank"} href={tool.link} className={"header-link d-flex align-items-center"}>
+                                                <img className={"mr-2"} src={tool.logo} alt="Logo du projet"/>
+                                                {tool.title}
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="container mt-3 mt-md-5">
+            <div className="row">
+                <div className="col d-flex flex-column align-items-center text-center">
+                    <h1>ClimateTracker</h1>
+                    <h2 className={"app-subtitle"}>Des outils numériques pour le climat qui permettent de s'approprier les ordres de grandeurs et se familiariser avec les objectifs de neutralité carbone.</h2>
+                </div>
+            </div>
+        </div>
+        <div className="container mt-5 pt-4">
+            <div className="row">
+                {tools.map(tool => (
+                    <div className="col-6">
+                        <a className="tool" href={tool.link} target={"_blank"} style={{'backgroundColor': tool.backgroundColor, 'color': tool.color}}>
+                            <div className={"text-center mb-3 mt-4"}>
+                                <img src={tool.logo} alt="Logo du projet"/>
+                            </div>
+                            <div>
+                                <h3 className={"tool-title mb-1"}>{tool.title}</h3>
+                                <p className={"tool-description mt-2"}>{tool.description}</p>
+                            </div>
+                        </a>
+                    </div>
+                ))}
+            </div>
+        </div>
     </div>
   );
 }
