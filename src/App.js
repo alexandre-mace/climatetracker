f9bd406183316385f62dfdfc99ff7b7f0d595e3b
Alex <contact.alexandremace@gmail.com>
2021-12-19 15:39:32 +0100

    Add burger menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import './App.css';
+import { DarkModeToggle } from "react-dark-mode-toggle-2";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(() => localStorage.getItem('climate-tracker-dark-mode') === 'true');
+
+  useEffect(() => {
+      if (isDarkMode) {
+          document.body.classList.add('dark-mode')
+      } else {
+          document.body.classList.remove('dark-mode')
+      }
+
+      localStorage.setItem('climate-tracker-dark-mode', isDarkMode.toString())
+  }, [isDarkMode]);
+
   const tools = [
       {
           title: 'Footprint',
@@ -13,17 +27,22 @@ function App() {
   ]
 
   return (
-    <div className="App">
+    <div className={"App"}>
         <div className="container header">
             <div className="row align-items-center">
                 <div className="col d-flex justify-content-between">
                     <a href={"/"} className={"d-flex align-items-center"}>
-                            <img className={"logo-img mr-3"} src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/thermometer_1f321-fe0f.png" alt="Thermomètre"/> ClimateTracker
+                            <img height={27} width={27} className={"logo-img mr-3"} src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/thermometer_1f321-fe0f.png" alt="Thermomètre"/> ClimateTracker
                     </a>
-                    <div>
-                        <div className={"header-links-trigger"}>
+                    <nav className={"d-none d-md-flex align-items-center"}>
+                            <DarkModeToggle
+                                onChange={setIsDarkMode}
+                                isDarkMode={isDarkMode}
+                                size={55}
+                            />
+                        <div className={"header-links-trigger ml-3"}>
                             <div className={"d-flex align-items-center"}>
-                                <div className={"mr-2"}>Les outils</div>
+                                <div className={"mr-2 text-nowrap"}>Les outils</div>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 6 6"
                                      className="Navigation_pageArrow__3E5fm" role="presentation">
                                     <path d="M.371 2.995A.6.6 0 01.822 2h4.356a.6.6 0 01.451.995L3.602 5.312a.8.8 0 01-1.204 0z"
@@ -32,10 +51,10 @@ function App() {
                             </div>
                             <div className="header-links-wrapper">
                                 <ul className={"header-links"}>
-                                    {tools.map(tool => (
-                                        <li>
-                                            <a target={"blank"} href={tool.link} className={"header-link d-flex align-items-center"}>
-                                                <img className={"mr-2"} src={tool.logo} alt="Logo du projet"/>
+                                    {tools.map((tool, index) => (
+                                        <li key={index}>
+                                            <a rel="noreferrer noopener" target={"blank"} href={tool.link} className={"header-link d-flex align-items-center"}>
+                                                <img height={25} width={25} className={"mr-2"} src={tool.logo} alt="Logo du projet"/>
                                                 {tool.title}
                                             </a>
                                         </li>
@@ -43,7 +62,27 @@ function App() {
                                 </ul>
                             </div>
                         </div>
-                    </div>
+                    </nav>
+                    <nav className={"d-block d-md-none"} role="navigation">
+                        <div id="menuToggle">
+                            <input type="checkbox" />
+
+                            <span></span>
+                            <span></span>
+                            <span></span>
+
+                            <ul id="menu">
+                                {tools.map((tool, index) => (
+                                    <li key={index}>
+                                        <a rel="noreferrer noopener" target={"blank"} href={tool.link} className={"header-link d-flex align-items-center"}>
+                                            <img height={25} width={25} className={"mr-2"} src={tool.logo} alt="Logo du projet"/>
+                                            {tool.title}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </nav>
                 </div>
             </div>
         </div>
@@ -55,13 +94,13 @@ function App() {
                 </div>
             </div>
         </div>
-        <div className="container mt-5 pt-4">
+        <div className="container mt-3 mt-md-5 pt-4">
             <div className="row">
-                {tools.map(tool => (
-                    <div className="col-6">
-                        <a className="tool" href={tool.link} target={"_blank"} style={{'backgroundColor': tool.backgroundColor, 'color': tool.color}}>
+                {tools.map((tool, index) => (
+                    <div className="col-12 col-md-6" key={index}>
+                        <a rel="noreferrer noopener" className="tool p-4 p-md-5" href={tool.link} target={"_blank"} style={{'backgroundColor': tool.backgroundColor, 'color': tool.color}}>
                             <div className={"text-center mb-3 mt-4"}>
-                                <img src={tool.logo} alt="Logo du projet"/>
+                                <img height={130} width={130} src={tool.logo} alt="Logo du projet"/>
                             </div>
                             <div>
                                 <h3 className={"tool-title mb-1"}>{tool.title}</h3>
